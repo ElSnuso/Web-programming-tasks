785769cabc0de8af566018974f2b18d0cc4de8ce
ElSnuso <amk1013371@student.hamk.fi>
2026-02-02 15:18:21 +0200

    change

---
+++ src/ex1.php
@@ -0,0 +1,44 @@
+<html>
+    <title>
+        <h1> Exercise 1: Getting Started with PHP - Allan Jakubovits</h1>
+    </title>
+<?php
+echo"<h3> Hello world! My name is \"David\" </h3>";
+?>
+    
+<?php
+$title = "PHP is intresting";
+?>
+
+<h4> <?php echo $title; ?> </h4>
+<?php
+$g1 = 5;
+$g2 = 4;
+$g3 = 5;
+?>
+<table border="1">
+    <tr>
+        <th>S.n.</th>
+        <th>Name</th>
+        <th>Grade</th>
+    </tr>
+    <tr>
+        <td>1</td>
+        <td>John</td>
+        <td><?php echo $g1; ?></td>
+    </tr>
+    <tr>
+        <td>2</td>
+        <td>Alice</td>
+        <td><?php echo $g2; ?></td>
+    </tr>
+    <tr>
+        <td>3</td>
+        <td>Bob</td>
+        <td><?php echo $g3; ?></td>
+    </tr>
+</table>
+<br>
+<img src="image.png" width = 700>
+
+</html>
